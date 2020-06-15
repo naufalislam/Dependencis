d3e7b48f125801f5e1c5890c0620e02992693d99
naufalislam <naufalislam.16@gmail.com>
2020-06-15 04:40:54 -0700

    dependencis

---
+++ app/src/main/java/com/dependencis/data/NetworkConfig.kt
@@ -0,0 +1,28 @@
+package com.dependencis.data
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+class NetworkConfig {// set interceptor
+fun getInterceptor() : OkHttpClient {
+    val logging = HttpLoggingInterceptor()
+    logging.level = HttpLoggingInterceptor.Level.BODY
+    val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+    return okHttpClient
+} fun getRetrofit() : Retrofit {
+    return Retrofit.Builder()
+        .baseUrl("https://jsonplaceholder.typicode.com/")
+        .client(getInterceptor())
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+} fun getService() =
+    getRetrofit().create(Users::class.java)}interface Users {
+    @GET("users")
+    fun getUsers(): Call<List<ResultUsers>>
+}
